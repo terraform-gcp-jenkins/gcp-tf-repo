a4b015763009dbc616781f003e3039d9df1161c9
Harindra <106613144+rrr-kgf@users.noreply.github.com>
2022-07-11 20:59:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "google" {
    // credentials = "${file("permission.json")}"
    
-     credentials = "$file("permission.json")"
+     credentials = "${file("permission.json")}"
     project     = var.project_id
     region      = var.reg
     zone        = var.zone
